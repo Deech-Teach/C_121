c6b0ee52f53a90196173e0a7c8fe22ac80b425ed
Deech-Teach <76897436+Deech-Teach@users.noreply.github.com>
2023-04-02 13:41:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,31 +6,25 @@ function setup() {
   classifier = ml5.imageClassifier('MobileNet',modelLoaded);
 }
 
-function modelLoaded()
-{
-  console.log("Model Loaded!");
+function modelLoaded() {
+    console.log('Model Loaded!');
 }
 
-function draw()
-{
-  image(video,0,0,300,300);
-  classifier.classify(video,gotResult);
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
 }
-
 var previous_result = '';
 
-function gotResult(error,results)
-{
-  if(error)
-  {
-    console.error(error)
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
   } else {
-    if((results[0].confidence > 0.5 ) && (previous_result != results[0].label))
-    {
+    if((results[0].confidence > 0.5) && (previous_result != results[0].label)){
       console.log(results);
       previous_result = results[0].label;
-      var synth = window.speechSyenthesis;
-      speak_data = 'Object Detected is - ' + results[0].label;
+      var synth = window.speechSynthesis;
+      speak_data = 'Object detected is - '+results[0].label;
       var utterThis = new SpeechSynthesisUtterance(speak_data);
       synth.speak(utterThis);
 
@@ -38,5 +32,4 @@ function gotResult(error,results)
       document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
   }
-  
 }
